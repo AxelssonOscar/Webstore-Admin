94ef974137bf4defb85bf56a677d70ec2b981b41
Oscar Axelsson <100521490+AxelssonOscar@users.noreply.github.com>
2022-05-16 14:52:40 +0200

    init

---
+++ Webstore-Admin/Webstore-Admin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Webstore_Admin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
